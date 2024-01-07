0bea2ab70c3a0f05586266125bef367ee67c3133
Ankita Tanwar <tanwarankita27@gmail.com>
2024-01-07 17:36:43 +0530

    Final commit

--- public/js/map.js
+++ public/js/map.js
@@ -5,10 +5,12 @@ const map = new mapboxgl.Map({
   zoom: 10,
 });
 
-
-
 // Create a default Marker and add it to the map.
 const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(listing.geometry.coordinates) 
-  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${listing.location}</h4><p>Exact location provided after booking!</p>`))
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.location}</h4><p>Exact location provided after booking!</p>`
+    )
+  )
   .addTo(map);
